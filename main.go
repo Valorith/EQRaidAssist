484f767b415cdaa9b52020323126b0b43ccc6f0b
Vayle <rgagnier06@gmail.com>
2022-02-12 22:29:35 -0500

    First implementation of the fileScanner

--- main.go
+++ main.go
@@ -7,12 +7,26 @@ import (
 	"path/filepath"
 	"strconv"
 	"strings"
+	"time"
 
 	loadFile "github.com/Valorith/EQRaidAssist/loadFIle"
 )
 
+//Global Variables
+var lastLoadedRaidFile string = ""
+
 func main() {
-	players := loadPlayers() // loads players from the newest Raid Dump file in the EQ directory
+	LoadedRaidFile, err := getNewestRaidFile()
+	lastLoadedRaidFile = LoadedRaidFile
+	if err != nil {
+		fmt.Println("lastLoadedRaidFile:", err)
+	}
+
+	//Load Players in
+	players, err := loadPlayers(lastLoadedRaidFile) // loads players from the newest Raid Dump file in the EQ directory
+	if err != nil {
+		fmt.Println("Player load failed: ", err)
+	}
 	playerCount := len(players)
 	if playerCount > 0 {
 		fmt.Printf("%d Players Succesfully loaded.\n", playerCount)
@@ -23,6 +37,10 @@ func main() {
 	for _, player := range players {
 		player.PrintPlayer()
 	}
+	//fScanner := FileScanner{LoadedRaidFile, false}
+	//Start Scanning
+	//fScanner.Scan()
+
 }
 
 type Player struct {
@@ -33,21 +51,26 @@ type Player struct {
 	Loot  []string
 }
 
-func loadPlayers() []Player {
+type FileScanner struct {
+	fileLocation string
+	enabled      bool
+}
+
+func getNewestRaidFile() (string, error) {
 	EQpath, err := os.Getwd()
 	if err != nil {
 		log.Println(err)
+		return "", fmt.Errorf("getwd: %w", err)
 	}
 	//logsFolder := EQpath + "\\Logs"
-	fmt.Println("Loading Players from: ", EQpath)
+	//fmt.Println("Loading Players from: ", EQpath)
 	raidDumpFileList := []string{}
 	fileListIndex := 0
 	filePathError := filepath.Walk(EQpath, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
-			fmt.Println(err)
-			return err
+			return fmt.Errorf("filepath.Walk: %w", err)
 		}
-		fmt.Printf("Scanning File: %s....\n", path)
+		//fmt.Printf("Scanning File: %s....\n", path)
 		isDir := info.IsDir()
 		itemPath := path
 		if !isDir {
@@ -61,13 +84,16 @@ func loadPlayers() []Player {
 
 	if filePathError != nil {
 		fmt.Println(filePathError)
+		return "", fmt.Errorf("filepath.Walk: %w", filePathError)
 	}
 
-	oldestIndex := 0
-	//newestIndex := len(raidDumpFileList) - 1
-	loadFileDirectory := raidDumpFileList[oldestIndex]
-	fmt.Println("Newest Raid Dump File Detected: ", loadFileDirectory)
-	var dumpLines []string = loadFile.Load(loadFileDirectory)
+	newestIndex := len(raidDumpFileList) - 1
+	return raidDumpFileList[newestIndex], nil
+}
+
+func loadPlayers(fileLocation string) ([]Player, error) {
+	fmt.Println("Newest Raid Dump File Detected: ", fileLocation)
+	var dumpLines []string = loadFile.Load(fileLocation)
 	var players []Player
 
 	for _, line := range dumpLines {
@@ -83,7 +109,7 @@ func loadPlayers() []Player {
 		players = append(players, Player{charName, charLevel, charClass, groupNumber, []string{}})
 	}
 
-	return players
+	return players, nil
 }
 
 func (player *Player) PrintPlayer() {
@@ -105,5 +131,46 @@ func (player *Player) AddLoot(lootItem string) {
 		player.Loot = append(player.Loot, lootItem)
 	} else {
 		fmt.Println("Error adding loot item: ", lootItem)
+	}
+}
+
+func (scanner *FileScanner) Scan() error {
+	// Ensure Scanner fileLocation is set
+	if scanner.fileLocation == "" {
+		return nil
+	}
+	scanner.enabled = true // Ensure scanner is enabled
+	for {
+		if scanner.enabled {
+			fmt.Println("Scanning File: ", scanner.fileLocation)
+			loadedRaidFile, err := getNewestRaidFile()
+			if err != nil {
+				return err
+			}
+
+			if loadedRaidFile != lastLoadedRaidFile {
+				//Load Players in
+				players, err := loadPlayers(loadedRaidFile) // loads players from the newest Raid Dump file in the EQ directory
+				if err != nil {
+					fmt.Println("Player load failed: ", err)
+				}
+				playerCount := len(players)
+				if playerCount > 0 {
+					fmt.Printf("%d Players Succesfully loaded.\n", playerCount)
+				} else {
+					fmt.Println("Player Loading Failed!")
+				}
+
+				for _, player := range players {
+					player.PrintPlayer()
+				}
+				lastLoadedRaidFile = loadedRaidFile
+			} else {
+				fmt.Println("No new raid file detected.")
+			}
+			time.Sleep(time.Second * 5)
+		} else {
+			return nil
+		}
 	}
 }
